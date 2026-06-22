b2b7db51d756b92ecd12ac1e2296eb1774eb5735
IA Building Dev <dev@ia-building.com>
2026-06-22 21:53:34 +0000

    fix: corregir referencia residual _HubScreenState en HomeScreen

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HubScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
